24a14e215163e46979d5a9c28993985e1128bc1b
Larry <lian999111@gmail.com>
2020-09-13 23:18:02 +0800

    Add _find_histo_peaks()

--- vision/lane.py
+++ vision/lane.py
@@ -18,18 +18,22 @@ class LaneMarkingDetector(object):
           It requires lane markings to be detected in previous images.
     """
 
-    def __init__(self, M, warped_size=(400, 600), valid_mask=None):
+    def __init__(self, M, px_per_meter_x, px_per_meter_y, warped_size=(400, 600), valid_mask=None):
         """ 
         Constructor method. 
 
         Input:
-            M: np.array of 3x3 matrix for IPM transform
-            warped_size: image size tuple of IPM image (width, height).
-            valid_mask: np.array of booleans to mask out edge pixels caused by the border of the FOV.
+            M: Numpy.array of 3x3 matrix for IPM transform.
+            px_per_meter_x: Pixels per meters in x in the Bird's eye view.
+            px_per_meter_y: Pixels per meters in y in the Bird's eye view.
+            warped_size: Image size tuple of IPM image (width, height).
+            valid_mask: Numpy.array of booleans to mask out edge pixels caused by the border of the FOV.
         """
         self.ipm_tform = M  # ipm stands for inverse perspective mapping
         self.warped_size = warped_size
         self.valid_mask = valid_mask
+        self.px_per_meters_x = px_per_meter_x
+        self.px_per_meters_y = px_per_meter_y
 
     def _get_bev_edge(self, lane_image):
         """ 
@@ -56,3 +60,22 @@ class LaneMarkingDetector(object):
             edge_image = edge_image.astype(np.uint8) & self.valid_mask
 
         return edge_image
+
+    def _find_histo_peaks(histo, required_height=500):
+    """ Find at most 2 peaks as lane marking searching bases from histogram. """
+    # Find peaks above required height
+    peaks, _ = find_peaks(histogram, height=required_height)
+
+    # Remove peaks that are too close to their precedents
+    # diff = np.diff(peaks)
+    # delete_mask = np.concatenate(([False], diff < 40))
+    # peaks = np.delete(peaks, delete_mask)
+
+    # Find at most 2 peaks from the middle towards left and 2 towards right
+    half_idx = histo.shape[0]/2
+    left_base = peaks[peaks < half_idx][-1] if peaks[peaks < half_idx].size != 0 else None
+    right_base = peaks[peaks >= half_idx][0] if peaks[peaks >= half_idx].size != 0 else None
+
+    return left_base, right_base
+
+    
