f7790c6af7ea00ea60582465e1601de3a4a5dc9a
Larry <lian999111@gmail.com>
2020-09-21 02:45:07 +0800

    Add loop() for pole

--- vision/pole.py
+++ vision/pole.py
@@ -92,13 +92,14 @@ class PoleDetector(object):
         wrt the frame at the center of the front bumper.
 
         Input:
-            pole_bases_uv: Image coordiantes (u-v) of detected pole bases.
             z: Assumed z coordinates perpendicular to ground of corresponding points
         """
-
-        pole_bases_xyz = im2world_known_z(
-            self.H, self.x0, self.pole_bases_uv, z_world=z)
-        self.pole_bases_xy = pole_bases_xyz[0:2, :]
+        if self.pole_bases_uv is not None:
+            pole_bases_xyz = im2world_known_z(
+                self.H, self.x0, self.pole_bases_uv, z_world=z)
+            self.pole_bases_xy = pole_bases_xyz[0:2, :]
+        else:
+            self.pole_bases_xy = None
 
 
 def single(folder_name, image_idx):
@@ -149,28 +150,117 @@ def single(folder_name, image_idx):
 
     pole_bases_uv = pole_detector.pole_bases_uv
 
-    x_world = depth_image[pole_bases_uv[1], pole_bases_uv[0]] - dist_cam_to_fbumper
-    poles_gt_xyz = im2world_known_x(
-        pole_detector.H, pole_detector.x0, pole_detector.pole_bases_uv, x_world)
 
-    # Visualization
     ss_image_copy = ss_image.copy()
-    for base_coord in pole_detector.pole_bases_uv.T:
-        ss_image_copy = cv2.circle(
-            ss_image_copy, (base_coord[0], base_coord[1]), 10, color=[1, 0, 0], thickness=10)
-
     _, ax = plt.subplots(1, 2)
     ax[0].imshow(ss_image_copy)
-    # ax[1].axis('equal')
-    ax[1].plot(poles_xy_z0[1, :], poles_xy_z0[0, :], '.', label='z = 0')
-    ax[1].plot(poles_xy_z1[1, :], poles_xy_z1[0, :], '.', label='z = 1')
-    ax[1].plot(poles_gt_xyz[1, :], poles_gt_xyz[0, :], '.', label='GT')
+
+    if pole_bases_uv is not None:
+        x_world = depth_image[pole_bases_uv[1], pole_bases_uv[0]] - dist_cam_to_fbumper
+        poles_gt_xyz = im2world_known_x(
+            pole_detector.H, pole_detector.x0, pole_detector.pole_bases_uv, x_world)
+
+        # Visualization
+        for base_coord in pole_detector.pole_bases_uv.T:
+            ss_image_copy = cv2.circle(
+                ss_image_copy, (base_coord[0], base_coord[1]), 10, color=[1, 0, 0], thickness=10)
+
+        # ax[1].axis('equal')
+        ax[1].plot(poles_xy_z0[1, :], poles_xy_z0[0, :], '.', label='z = 0')
+        ax[1].plot(poles_xy_z1[1, :], poles_xy_z1[0, :], '.', label='z = 1')
+        ax[1].plot(poles_gt_xyz[1, :], poles_gt_xyz[0, :], '.', label='GT')
+        ax[1].set_xlim((30, -30))
+        ax[1].set_ylim((-5, 60))
+
+        plt.legend()
+    plt.show()
+
+def loop(folder_name):
+    argparser = argparse.ArgumentParser(
+        description='Pole Detection using Semantic Images')
+    argparser.add_argument('config', type=argparse.FileType(
+        'r'), help='configuration yaml file for carla env setup')
+    argparser.add_argument('vision_config', type=argparse.FileType(
+        'r'), help='configuration yaml file for vision algorithms')
+    args = argparser.parse_args()
+
+    # Read configurations from yaml file
+    with args.config as config_file:
+        config_args = yaml.safe_load(config_file)
+    with args.vision_config as vision_config_file:
+        vision_config_args = yaml.safe_load(vision_config_file)
+
+    dist_cam_to_fbumper = (config_args['ego_veh']['raxle_to_fbumper']
+                           - config_args['sensor']['front_camera']['pos_x']
+                           - config_args['ego_veh']['raxle_to_cg'])
+
+    # Load parameters for inverse projection
+    calib_data = np.load('vision/calib_data.npz')
+    K = calib_data['K']
+    R = calib_data['R']
+    x0 = calib_data['x0']
+
+    # Load data
+    mydir = os.path.join('recordings', folder_name)
+    with open(os.path.join(mydir, 'ss_images'), 'rb') as image_file:
+        ss_images = pickle.load(image_file)
+    with open(os.path.join(mydir, 'depth_buffers'), 'rb') as image_file:
+        depth_buffers = pickle.load(image_file)
+
+
+    pole_detector = PoleDetector(K, R, x0, vision_config_args['pole'])
+
+    _, ax = plt.subplots(1, 2)
+    im = ax[0].imshow(np.ones(ss_images[0].shape).astype(np.uint8), vmin=0, vmax=12)
+    pole0 = ax[1].plot([], [], '.', label='z = 0')[0]
+    pole1 = ax[1].plot([], [], '.', label='z = 1')[0]
+    pole_gt = ax[1].plot([], [], '.', label='GT')[0]
     ax[1].set_xlim((30, -30))
     ax[1].set_ylim((-5, 60))
-
     plt.legend()
-    plt.show()
+    plt.show(block=False)
+
+    # Loop over data
+    for image_idx, (ss_image, depth_buffer) in enumerate(zip(ss_images, depth_buffers)):
+        # Extract pole-relevant semantic labels
+        pole_image = (ss_image == 5).astype(np.uint8)
+
+        depth_image = utils.decode_depth(depth_buffer)
+
+        ss_image_copy = ss_image.copy()
+
+        pole_detector.update_poles(pole_image, z=0)
+        poles_xy_z0 = pole_detector.pole_bases_xy
+        pole_detector.update_poles(pole_image, z=0.1)
+        poles_xy_z1 = pole_detector.pole_bases_xy
+
+        pole_bases_uv = pole_detector.pole_bases_uv
+
+        if pole_bases_uv is not None:
+            # Ground truth
+            x_world = depth_image[pole_bases_uv[1], pole_bases_uv[0]] - dist_cam_to_fbumper
+            poles_gt_xyz = im2world_known_x(
+                pole_detector.H, pole_detector.x0, pole_detector.pole_bases_uv, x_world)
+
+            # Visualization
+            for base_coord in pole_detector.pole_bases_uv.T:
+                ss_image_copy = cv2.circle(
+                    ss_image_copy, (base_coord[0], base_coord[1]), 10, color=[1, 0, 0], thickness=10)
+
+            
+            pole0.set_data(poles_xy_z0[1, :], poles_xy_z0[0, :])
+            pole1.set_data(poles_xy_z1[1, :], poles_xy_z1[0, :])
+            pole_gt.set_data(poles_gt_xyz[1, :], poles_gt_xyz[0, :])
+        else:
+            pole0.set_data([], [])
+            pole1.set_data([], [])
+            pole_gt.set_data([], [])
+
+        im.set_data(ss_image_copy)
+        ax[1].set_title(image_idx)
+        plt.pause(0.001)
 
 
 if __name__ == "__main__":
-    single('depth', 20)
+    # single('depth', 193)
+    loop('depth')
