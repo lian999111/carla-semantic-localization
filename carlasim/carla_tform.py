5cc9997ade176764dc56d4ac83e526a52547eec7
Larry <lian999111@gmail.com>
2020-10-09 14:35:51 +0800

    Add e2w transform functionalities
    Refactor CarlaW2ETfrom in to Transform which is more general
    in that it now provides functionalities to transform point from ego to
    world frame.

--- carlasim/carla_tform.py
+++ carlasim/carla_tform.py
@@ -20,12 +20,12 @@ import numpy as np
 from scipy.spatial.transform import Rotation
 
 
-class CarlaW2ETform:
+class Transform:
     """ 
-    Helper class to perform world-to-ego transformation for Carla. 
+    Helper class to perform world-to-ego (w2e) and ego-to-world (e2w) transformations. 
 
     Carla provides only an API to transform a location from ego to world frame.
-    This class complements the transfromation abilities from world to ego frame.
+    This class provides functionalities to perform both w2e and e2w transfromations.
 
     Important note:
      Carla (Unreal Engine) uses:
@@ -33,7 +33,7 @@ class CarlaW2ETform:
      - Right-handed z-down coordinate (airplane like) coordinate system for rotations (roll-pitch-yaw)
 
     This class automatically converts given carla.Transform and carla.Location so the transformation results
-    will be in our right-handed z-up coordinate system convention.
+    will always be in the right-handed z-up coordinate system convention.
     """
 
     def __init__(self, carla_tform: carla.Transform):
@@ -44,10 +44,14 @@ class CarlaW2ETform:
             carla_tform: Carla.Transform object of ego frame.
         """
         self._carla_tform = carla_tform
-        # rotation matrix to transform a vector from world frame to ego frame
+        # rotation matrix to rotate a point from world to ego frame
         self._rotm_w2e = None
-        # homogeneous transformation matrix to transform a vector from world frame to ego frame
+        # homogeneous transformation matrix to transform a point from world frame to ego frame
         self._tform_w2e = None
+        # rotation matrix to rotate a point from ego frame to world frame
+        self._rotm_e2w = None
+        # homogeneous transformation matrix to transform a point from ego frame to world frame
+        self._tform_e2w = None
 
     @classmethod
     def from_conventional(cls, location, orientation):
@@ -84,14 +88,14 @@ class CarlaW2ETform:
 
     def rot_w2e_carla_vector3D(self, vector3D: carla.Vector3D):
         """ 
-        Rotate the given carla.Vector3D in world frame into ego frame.
+        Rotate the given carla.Vector3D from world to ego frame.
 
         Note the return np 3D vector already follows the right-handed z-up coordinate system.
 
         Input:
             vector3D: Carla.Vector3D which follows carla's coordinate system (z-down system).
         Output:
-            Numpy.array representing a 3D point in the right-handed z-up coordinate system.
+            1D Numpy.array representing a 3D point in the right-handed z-up coordinate system.
         """
         if self._rotm_w2e is None:
             self._init_rotm_w2e()
@@ -101,14 +105,14 @@ class CarlaW2ETform:
 
     def tform_w2e_carla_vector3D(self, vector3D: carla.Vector3D):
         """ 
-        Homogeneous transform the given carla.Vector3D in world frame into ego frame.
+        Homogeneous transform the given carla.Vector3D from world to ego frame.
 
         Note the return np 3D vector already follows right-handed z-up coordinate system.
 
         Input:
             vector3D: Carla.Vector3D which follows carla's coordinate system (z-down system).
         Output:
-            Numpy.array representing a 3D point in the right-handed z-up coordinate system.
+            1D Numpy.array representing a 3D point in the right-handed z-up coordinate system.
         """
         if self._tform_w2e is None:
             self._init_tform_w2e()
@@ -118,7 +122,7 @@ class CarlaW2ETform:
 
     def rot_w2e_numpy_array(self, np_points):
         """
-        Rotate the given set of numpy points in world frame into ego frame.
+        Rotate the given set of numpy points from world to ego frame.
 
         Note the input should follows the right-handed z-up coordinate system.
 
@@ -137,7 +141,7 @@ class CarlaW2ETform:
 
     def tform_w2e_numpy_array(self, np_points):
         """
-        Transform the given set of numpy points in world frame into ego frame.
+        Transform the given set of numpy points from world to ego frame.
 
         Note the input should follows the right-handed z-up coordinate system.
 
@@ -158,8 +162,88 @@ class CarlaW2ETform:
         np_homo_vec = np.concatenate((np_points, np.ones((1, n_pts))), axis=0)
         return (self._tform_w2e @ np_homo_vec)[0:3, :]
 
+    def rot_e2w_carla_vector3D(self, vector3D: carla.Vector3D):
+        """ 
+        Rotate the given carla.Vector3D from ego to world frame.
+
+        Note the return np 3D vector already follows the right-handed z-up coordinate system.
+
+        Input:
+            vector3D: Carla.Vector3D which follows carla's coordinate system (z-down system).
+        Output:
+            1D Numpy.array representing a 3D point in the right-handed z-up coordinate system.
+        """
+        if self._rotm_e2w is None:
+            self._init_rotm_e2w()
+        # Need to convert from left-handed to right-handed before apply the rotation
+        np_vec = np.array([vector3D.x, -vector3D.y, vector3D.z])
+        return self._rotm_e2w.dot(np_vec)
+
+    def tform_e2w_carla_vector3D(self, vector3D: carla.Vector3D):
+        """ 
+        Homogeneous transform the given carla.Vector3D from ego to world frame.
+
+        Note the return np 3D vector already follows right-handed z-up coordinate system.
+
+        Input:
+            vector3D: Carla.Vector3D which follows carla's coordinate system (z-down system).
+        Output:
+            1D Numpy.array representing a 3D point in the right-handed z-up coordinate system.
+        """
+        if self._tform_e2w is None:
+            self._init_tform_e2w()
+        # Need to convert from left-handed to right-handed before apply the homogeneous transformation
+        np_homo_vec = np.array([vector3D.x, -vector3D.y, vector3D.z, 1])
+        return self._tform_e2w.dot(np_homo_vec)[0:3]
+
+    def rot_e2w_numpy_array(self, np_points):
+        """
+        Rotate the given set of numpy points from ego to world frame.
+
+        Note the input should follows the right-handed z-up coordinate system.
+
+        Input:
+            np_points: 3-by-N numpy array, where each column is the (x, y, z) 
+                       coordinates of a point.
+        Returns:
+            An 3-by-N numpy array of rotated points in the right-handed z-up coordinate system.
+        """
+        # Make sure shape is 2D
+        if np_points.ndim != 2:
+            np_points = np_points.reshape((3, -1))    # 3-by-N
+        if self._rotm_e2w is None:
+            self._init_rotm_e2w()
+        return self._rotm_e2w @ np_points
+
+    def tform_e2w_numpy_array(self, np_points):
+        """
+        Transform the given set of numpy points from ego to world frame.
+
+        Note the input should follows the right-handed z-up coordinate system.
+
+        Input:
+            np_points: 3-by-N numpy array, where each column is the (x, y, z) 
+                       coordinates of a point.
+        Returns:
+            An 3-by-N numpy array of rotated points in the right-handed z-up coordinate system.
+        """
+        if np_points.ndim != 2:
+            np_points = np_points.reshape((3, -1))    # 3-by-N
+        if self._tform_e2w is None:
+            self._init_tform_e2w()
+
+        # Number of points
+        n_pts = np_points.shape[1]
+
+        np_homo_vec = np.concatenate((np_points, np.ones((1, n_pts))), axis=0)
+        return (self._tform_e2w @ np_homo_vec)[0:3, :]
+
     def _init_rotm_w2e(self):
         """ Helper method to create rotation matrix _rotm_w2e. """
+        if self._rotm_e2w is not None:
+            self._rotm_w2e = self._rotm_e2w.T
+            return
+
         rotation = self._carla_tform.rotation
         # Need to convert from right-handed z-down to right-handed z-up system when building up the rotation matrix
         # Transpose so it is the rotation of the world frame wrt the ego frame
@@ -175,5 +259,28 @@ class CarlaW2ETform:
         self._tform_w2e[0:3, 0:3] = self._rotm_w2e
         trvec = np.array([self._carla_tform.location.x,
                           -self._carla_tform.location.y,
-                          -self._carla_tform.location.z]).T
+                          -self._carla_tform.location.z])
         self._tform_w2e[0:3, 3] = - self._rotm_w2e.dot(trvec)
+    
+    def _init_rotm_e2w(self):
+        """ Helper method to create rotation matrix _rotm_e2w. """
+        if self._rotm_w2e is not None:
+            self._rotm_e2w = self._rotm_w2e.T
+            return
+
+        rotation = self._carla_tform.rotation
+        # Need to convert from right-handed z-down to right-handed z-up system when building up the rotation matrix
+        # Transpose so it is the rotation of the world frame wrt the ego frame
+        self._rotm_e2w = Rotation.from_euler(
+            'zyx', [-rotation.yaw, -rotation.pitch, rotation.roll], degrees=True).as_matrix()
+
+    def _init_tform_e2w(self):
+        """ Helper method to create rotation matrix _tform_e2w. """
+        if self._rotm_e2w is None:
+            self._init_rotm_e2w()
+        self._tform_e2w = np.zeros((4, 4), dtype=np.float)
+        self._tform_e2w[3, 3] = 1
+        self._tform_e2w[0:3, 0:3] = self._rotm_e2w
+        self._tform_e2w[0:3, 3] = np.array([self._carla_tform.location.x,
+                          -self._carla_tform.location.y,
+                          -self._carla_tform.location.z])
--- carlasim/test/test_carla_tform.py
+++ carlasim/test/test_carla_tform.py
@@ -15,10 +15,11 @@ import carla
 import unittest
 import numpy as np
 
-from carlasim.carla_tform import CarlaW2ETform
+from carlasim.carla_tform import Transform
 
 
 class TestCarlaW2ETform(unittest.TestCase):
+    # W2E
     def test_rot_w2e_carla_vector3D(self):
         # In left-handed z-up coordinate system
         location = carla.Location(10, 0, 0)
@@ -26,13 +27,13 @@ class TestCarlaW2ETform(unittest.TestCase):
         rotation = carla.Rotation(yaw=90)
         carla_transform = carla.Transform(location, rotation)
 
-        w2e_tform = CarlaW2ETform(carla_transform)
+        tform = Transform(carla_transform)
 
         # In left-handed z-up coordinate system
         location_in_world = carla.Location(0, 10, 0)
 
         # In right-handed z-up coordinate system
-        np_pt_in_ego = w2e_tform.rot_w2e_carla_vector3D(location_in_world)
+        np_pt_in_ego = tform.rot_w2e_carla_vector3D(location_in_world)
 
         self.assertAlmostEqual(np_pt_in_ego[0], 10.0)
         self.assertAlmostEqual(np_pt_in_ego[1], 0.0)
@@ -45,97 +46,203 @@ class TestCarlaW2ETform(unittest.TestCase):
         rotation = carla.Rotation(yaw=90)
         carla_transform = carla.Transform(location, rotation)
 
-        w2e_tform = CarlaW2ETform(carla_transform)
+        tform = Transform(carla_transform)
 
         # In left-handed z-up coordinate system
         location_in_world = carla.Location(0, 10, 0)
 
         # In right-handed z-up coordinate system
-        np_pt_in_ego = w2e_tform.tform_w2e_carla_vector3D(location_in_world)
+        np_pt_in_ego = tform.tform_w2e_carla_vector3D(location_in_world)
 
         self.assertAlmostEqual(np_pt_in_ego[0], 10.0)
         self.assertAlmostEqual(np_pt_in_ego[1], -10.0)
         self.assertAlmostEqual(np_pt_in_ego[2], 0.0)
 
-    def test_rot_w2e_carla_numpy_point(self):
+    def test_rot_w2e_numpy_point(self):
         # In left-handed z-up coordinate system
         location = carla.Location(10, 0, 0)
         # In right-handed z-down coordinate system
         rotation = carla.Rotation(yaw=90)
         carla_transform = carla.Transform(location, rotation)
 
-        w2e_tform = CarlaW2ETform(carla_transform)
+        tform = Transform(carla_transform)
 
         # In right-handed z-up coordinate system
         np_pts_in_world = np.array([0, -10, 0])
         # In right-handed z-up coordinate system
-        np_pts_in_ego = w2e_tform.rot_w2e_numpy_array(np_pts_in_world)
+        np_pts_in_ego = tform.rot_w2e_numpy_array(np_pts_in_world)
 
         np.testing.assert_array_almost_equal(
             np_pts_in_ego, np.array([10, 0, 0]).reshape((3, -1)))
 
-    def test_rot_w2e_carla_numpy_points(self):
+    def test_rot_w2e_numpy_points(self):
         # In left-handed z-up coordinate system
         location = carla.Location(10, 0, 0)
         # In right-handed z-down coordinate system
         rotation = carla.Rotation(yaw=90)
         carla_transform = carla.Transform(location, rotation)
 
-        w2e_tform = CarlaW2ETform(carla_transform)
+        tform = Transform(carla_transform)
 
         # In right-handed z-up coordinate system
         np_pts_in_world = np.array([[0, -10, 0], [10, 10, 0]]).T
         # In right-handed z-up coordinate system
-        np_pts_in_ego = w2e_tform.rot_w2e_numpy_array(np_pts_in_world)
+        np_pts_in_ego = tform.rot_w2e_numpy_array(np_pts_in_world)
 
         np.testing.assert_array_almost_equal(
             np_pts_in_ego, np.array([[10, 0, 0], [-10, 10, 0]]).T.reshape((3, -1)))
 
-    def test_tform_w2e_carla_numpy_point(self):
+    def test_tform_w2e_numpy_point(self):
         # In left-handed z-up coordinate system
         location = carla.Location(10, 0, 0)
         # In right-handed z-down coordinate system
         rotation = carla.Rotation(yaw=90)
         carla_transform = carla.Transform(location, rotation)
 
-        w2e_tform = CarlaW2ETform(carla_transform)
+        tform = Transform(carla_transform)
 
         # In right-handed z-up coordinate system
         np_pts_in_world = np.array([0, -10, 0])
         # In right-handed z-up coordinate system
-        np_pts_in_ego = w2e_tform.tform_w2e_numpy_array(np_pts_in_world)
+        np_pts_in_ego = tform.tform_w2e_numpy_array(np_pts_in_world)
 
         np.testing.assert_array_almost_equal(
             np_pts_in_ego, np.array([10, -10, 0]).reshape((3, -1)))
 
-    def test_tform_w2e_carla_numpy_points(self):
+    def test_tform_w2e_numpy_points(self):
         # In left-handed z-up coordinate system
         location = carla.Location(10, 0, 0)
         # In right-handed z-down coordinate system
         rotation = carla.Rotation(yaw=90)
         carla_transform = carla.Transform(location, rotation)
 
-        w2e_tform = CarlaW2ETform(carla_transform)
+        tform = Transform(carla_transform)
 
         # In right-handed z-up coordinate system
         np_pts_in_world = np.array([[0, -10, 0], [10, 10, 0]]).T
         # In right-handed z-up coordinate system
-        np_pts_in_ego = w2e_tform.tform_w2e_numpy_array(np_pts_in_world)
+        np_pts_in_ego = tform.tform_w2e_numpy_array(np_pts_in_world)
 
         np.testing.assert_array_almost_equal(
             np_pts_in_ego, np.array([[10, -10, 0], [-10, 0, 0]]).T.reshape((3, -1)))
 
+    # E2W
+    def test_rot_e2w_carla_vector3D(self):
+        # In left-handed z-up coordinate system
+        location = carla.Location(10, 0, 0)
+        # In right-handed z-down coordinate system
+        rotation = carla.Rotation(yaw=90)
+        carla_transform = carla.Transform(location, rotation)
+
+        tform = Transform(carla_transform)
+
+        # In left-handed z-up coordinate system
+        location_in_ego = carla.Location(10, 10, 0)
+
+        # In right-handed z-up coordinate system
+        np_pt_in_world = tform.rot_e2w_carla_vector3D(location_in_ego)
+
+        np.testing.assert_array_almost_equal(
+            np_pt_in_world, np.array([-10, -10, 0]))
+
+    def test_tform_e2w_carla_vector3D(self):
+        # In left-handed z-up coordinate system
+        location = carla.Location(10, 0, 0)
+        # In right-handed z-down coordinate system
+        rotation = carla.Rotation(yaw=90)
+        carla_transform = carla.Transform(location, rotation)
+
+        tform = Transform(carla_transform)
+
+        # In left-handed z-up coordinate system
+        location_in_ego = carla.Location(10, 10, 0)
+
+        # In right-handed z-up coordinate system
+        np_pt_in_world = tform.tform_e2w_carla_vector3D(location_in_ego)
+
+        np.testing.assert_array_almost_equal(
+            np_pt_in_world, np.array([0, -10, 0]))
+
+    def test_rot_e2w_numpy_point(self):
+        # In left-handed z-up coordinate system
+        location = carla.Location(10, 0, 0)
+        # In right-handed z-down coordinate system
+        rotation = carla.Rotation(yaw=90)
+        carla_transform = carla.Transform(location, rotation)
+
+        tform = Transform(carla_transform)
+
+        # In right-handed z-up coordinate system
+        np_pts_in_ego = np.array([0, 10, 0])
+        # In right-handed z-up coordinate system
+        np_pts_in_world = tform.rot_e2w_numpy_array(np_pts_in_ego)
+
+        np.testing.assert_array_almost_equal(
+            np_pts_in_world, np.array([10, 0, 0]).reshape((3, -1)))
+
+    def test_rot_e2w_numpy_points(self):
+        # In left-handed z-up coordinate system
+        location = carla.Location(10, 0, 0)
+        # In right-handed z-down coordinate system
+        rotation = carla.Rotation(yaw=90)
+        carla_transform = carla.Transform(location, rotation)
+
+        tform = Transform(carla_transform)
+
+        # In right-handed z-up coordinate system
+        np_pts_in_ego = np.array([[0, 10, 0], [10, -10, 0]]).T
+        # In right-handed z-up coordinate system
+        np_pts_in_world = tform.rot_e2w_numpy_array(np_pts_in_ego)
+
+        np.testing.assert_array_almost_equal(
+            np_pts_in_world, np.array([[10, 0, 0], [-10, -10, 0]]).T.reshape((3, -1)))
+
+    def test_tform_e2w_numpy_point(self):
+        # In left-handed z-up coordinate system
+        location = carla.Location(10, 0, 0)
+        # In right-handed z-down coordinate system
+        rotation = carla.Rotation(yaw=90)
+        carla_transform = carla.Transform(location, rotation)
+
+        tform = Transform(carla_transform)
+
+        # In right-handed z-up coordinate system
+        np_pts_in_ego = np.array([-10, 0, 0])
+        # In right-handed z-up coordinate system
+        np_pts_in_world = tform.tform_e2w_numpy_array(np_pts_in_ego)
+
+        np.testing.assert_array_almost_equal(
+            np_pts_in_world, np.array([10, 10, 0]).reshape((3, -1)))
+
+    def test_tform_e2w_numpy_points(self):
+        # In left-handed z-up coordinate system
+        location = carla.Location(10, 0, 0)
+        # In right-handed z-down coordinate system
+        rotation = carla.Rotation(yaw=90)
+        carla_transform = carla.Transform(location, rotation)
+
+        tform = Transform(carla_transform)
+
+        # In right-handed z-up coordinate system
+        np_pts_in_ego = np.array([[-10, 0, 0], [10, -10, 0]]).T
+        # In right-handed z-up coordinate system
+        np_pts_in_world = tform.tform_e2w_numpy_array(np_pts_in_ego)
+
+        np.testing.assert_array_almost_equal(
+            np_pts_in_world, np.array([[10, 10, 0], [0, -10, 0]]).T.reshape((3, -1)))
+
+    # Construct
     def test_from_conventional(self):
         # In right-handed z-up coordinate system
         conventional_location = [0, 10, 0]
         conventional_orientation = [0, 0, np.pi/2]
-        w2e_tform = CarlaW2ETform.from_conventional(
+        tform = Transform.from_conventional(
             conventional_location, conventional_orientation)
-        
+
         # In right-handed z-up coordinate system
         np_pts_in_world = np.array([0, -10, 0])
         # In right-handed z-up coordinate system
-        np_pts_in_ego = w2e_tform.tform_w2e_numpy_array(np_pts_in_world)
+        np_pts_in_ego = tform.tform_w2e_numpy_array(np_pts_in_world)
 
         np.testing.assert_array_almost_equal(
             np_pts_in_ego, np.array([-20, 0, 0]).reshape((3, -1)))
@@ -144,13 +251,13 @@ class TestCarlaW2ETform(unittest.TestCase):
         # In right-handed z-up coordinate system
         conventional_location = np.array([0, 10, 0]).reshape((3, 1))
         conventional_orientation = np.array([0, 0, np.pi/2]).reshape((3, 1))
-        w2e_tform = CarlaW2ETform.from_conventional(
+        tform = Transform.from_conventional(
             conventional_location, conventional_orientation)
-        
+
         # In right-handed z-up coordinate system
         np_pts_in_world = np.array([0, -10, 0])
         # In right-handed z-up coordinate system
-        np_pts_in_ego = w2e_tform.tform_w2e_numpy_array(np_pts_in_world)
+        np_pts_in_ego = tform.tform_w2e_numpy_array(np_pts_in_world)
 
         np.testing.assert_array_almost_equal(
             np_pts_in_ego, np.array([-20, 0, 0]).reshape((3, -1)))
