62ddad72dc278c68a591da76bda1759dba824c8d
Larry Liang <lian999111@gmail.com>
2020-11-24 16:58:18 +0800

    Add covariance computation into ctrv.py
    Former-commit-id: 94b70f9171440d4c4daa72cc3bd269c1132ce6b3

--- model/ctrv.py
+++ model/ctrv.py
@@ -1,28 +1,58 @@
 # Implementation of CTRV model
 
 from math import sin, cos
+import numpy as np
 
-def predict_motion_from_ego_frame(velocity, yaw_rate, dt):
+def predict_motion_from_ego_frame(vx, yaw_rate, delta_t, Q=None):
     """ 
     Predict the expected CTRV motion with respect to current ego frame given the control and time difference.
-    That is, get the expected CTRV motion while viewed from the current pose.
-    The control includes:
-        velocity: m/s
-        yaw rate: rad/s
     
+    That is, get the expected CTRV motion while viewed from the current pose.    
     When yaw rate is very small, CV model is used instead to avoid division-by-zero.
-    """
-    if abs(yaw_rate) < 1e-3:
-        # Use CV model
-        delta_x = velocity * dt
-        delta_y = 0
-        delta_theta = 0
-    else:
-        # CTRV
-        r = velocity / yaw_rate
-        yaw_rate_dt = yaw_rate * dt     # to avoid recomputing it
-        delta_x = r * sin(yaw_rate_dt)
-        delta_y = r * (-cos(yaw_rate_dt) + 1)
-        delta_theta = yaw_rate_dt
 
-    return delta_x, delta_y, delta_theta
+    Input:
+        vx:         velocity in x-axis of the ego frame. (m/s)
+        yaw_rate:   yaw rate. (rad/s)
+        delta_t:    time difference. (sec)
+        Q:          2-by-2 noise covariance matrix of vx and yaw_rate. 
+                    When given, the noise covariance matrix wrt delta_x, delta_y, and delta_theta is computed and returned.
+    Output:
+        delta_x, delta_y, delta_theta, cov
+    """
+    # Initialize L, the 3-by-2 Jacobian of displacement wrt noise in vx and yaw_rate
+    if Q:
+        L = np.zeros((3, 2))
+
+    if yaw_rate > 1e-3:
+        yaw_rate_T = yaw_rate * delta_t
+        r = vx/yaw_rate
+        delta_x = r * np.sin(yaw_rate_T)
+        delta_y = r * (-np.cos(yaw_rate_T) + 1)
+        delta_theta = yaw_rate_T
+
+        if Q:
+            L[0, 0] = 1/yaw_rate * np.sin(yaw_rate_T)
+            L[0, 1] = -r/yaw_rate * np.sin(yaw_rate_T) + r*delta_t*np.cos(yaw_rate_T)
+            L[1, 0] = 1/yaw_rate * (-np.cos(yaw_rate_T) + 1)
+            L[1, 1] = -r/yaw_rate * (-np.cos(yaw_rate_T) + 1) + r*delta_t*np.sin(yaw_rate_T)
+            L[2, 1] = delta_t
+    else:
+        delta_x = vx*delta_t
+        delta_y = 0.0
+
+        if Q:
+            L[0, 0] = delta_t
+        
+        if vx > 0.1:
+            delta_theta = yaw_rate * delta_t
+            if Q:
+                L[2, 1] = delta_t
+        else:
+            # Do not update theta if vx is small
+            delta_theta = 0.0
+    if Q:
+        cov = L @ Q @ L.T + np.eye(3)*0.000001     # add a small diagnol matrix to stabilize cov
+    else:
+        cov = None
+
+    return delta_x, delta_y, delta_theta, cov
--- model/test/test_ctrv.py
+++ model/test/test_ctrv.py
@@ -5,16 +5,25 @@ import model.ctrv as ctrv
 
 class TestCTRV(unittest.TestCase):
     def test_predict_motion_from_ego_frame_big_yaw_rate(self):
-        dx, dy, dtheta = ctrv.predict_motion_from_ego_frame(1, 1, 1)
+        dx, dy, dtheta, cov = ctrv.predict_motion_from_ego_frame(1, 1, 1)
         self.assertAlmostEqual(dx, 0.8414710)
         self.assertAlmostEqual(dy, 0.4596977)
         self.assertAlmostEqual(dtheta, 1.0)
+        self.assertIsNone(cov)
 
     def test_predict_motion_from_ego_frame_small_yaw_rate(self):
-        dx, dy, dtheta = ctrv.predict_motion_from_ego_frame(1, 1e-4, 1)
+        dx, dy, dtheta, cov = ctrv.predict_motion_from_ego_frame(1, 1e-4, 1)
         self.assertAlmostEqual(dx, 1.)
         self.assertAlmostEqual(dy, 0.)
+        self.assertAlmostEqual(dtheta, 1e-4)
+        self.assertIsNone(cov)
+
+    def test_predict_motion_from_ego_frame_small_yaw_rate_velocity(self):
+        dx, dy, dtheta, cov = ctrv.predict_motion_from_ego_frame(1e-4, 1e-4, 1)
+        self.assertAlmostEqual(dx, 1e-4)
+        self.assertAlmostEqual(dy, 0.)
         self.assertAlmostEqual(dtheta, 0.)
+        self.assertIsNone(cov)
 
 if __name__ is '__main__':
     unittest.main()
