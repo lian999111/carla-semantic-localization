b8b322228ff5b9202304c493ab86fc0a3009c16b
Larry Liang <lian999111@gmail.com>
2020-12-02 11:58:39 +0800

    Update comments
    Former-commit-id: 5576a4dd8225cc61c56dce25294e2a8dd998da55

--- localization/odom.py
+++ localization/odom.py
@@ -1,4 +1,4 @@
-# Implementation of CTRV odometry factor
+""" Implementation of CTRV odometry factor """
 
 import numpy as np
 from minisam import BetweenFactor, key, GaussianLoss, SE2, SO2
@@ -6,7 +6,9 @@ from minisam import BetweenFactor, key, GaussianLoss, SE2, SO2
 from model.ctrv import predict_motion_from_ego_frame
 
 def create_ctrv_between_factor(key1, key2, vx, yaw_rate, delta_t, ctrv_config):
-    # Compute noise covariance matrix
+    """Create a BetweenFactor based on CTRV motion model.
+    """
+    # Initizlize noise covariance matrix of vx and yaw_rate
     Q = np.zeros((2, 2))
     Q[0, 0] = ctrv_config['stddev_vx']**2
     Q[1, 1] = ctrv_config['stddev_yaw_rate']**2
--- model/ctrv.py
+++ model/ctrv.py
@@ -9,15 +9,23 @@ def predict_motion_from_ego_frame(vx, yaw_rate, delta_t, Q=None):
     
     That is, get the expected CTRV motion while viewed from the current pose.    
     When yaw rate is very small, CV model is used instead to avoid division-by-zero.
+    
+    Note: The resultant covarinace matrix of delta_x, delta_y, delta_theta is obtained by transforming 
+    the covariance matrix of vx and yaw rate based on the Jacobian of the CTRV model. This method results
+    in a rank-deficient matrix and causes problems during optimization. Thus, a small diagonal matrix is
+    added in order to make it full-rank and stabilize the optimization.
 
-    Input:
-        vx:         velocity in x-axis of the ego frame. (m/s)
-        yaw_rate:   yaw rate. (rad/s)
-        delta_t:    time difference. (sec)
-        Q:          2-by-2 noise covariance matrix of vx and yaw_rate. 
+    Args:
+        vx:         Velocity in x-axis of the ego frame. (m/s)
+        yaw_rate:   Yaw rate. (rad/s)
+        delta_t:    Time difference. (sec)
+        Q:          2-by-2 noise covariance matrix of vx and yaw_rate. Defaluts to None.
                     When given, the noise covariance matrix wrt delta_x, delta_y, and delta_theta is computed and returned.
-    Output:
-        delta_x, delta_y, delta_theta, cov
+    Return:
+        delta_x: Difference in x.
+        delta_y: Difference in y.
+        delta_theta: Difference in heading.
+        cov: 3-by-3 noise covariance matrix.
     """
     # Initialize L, the 3-by-2 Jacobian of displacement wrt noise in vx and yaw_rate
     if Q is not None:
