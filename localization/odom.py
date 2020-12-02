f3df4cf217ec80017addc6a1e4b8b4a26866811d
Larry Liang <lian999111@gmail.com>
2020-12-02 12:22:48 +0800

    Update docstring
    Former-commit-id: ee134766836dc5c3bb6a9bf4294961da436f1f47

--- localization/odom.py
+++ localization/odom.py
@@ -5,8 +5,18 @@ from minisam import BetweenFactor, key, GaussianLoss, SE2, SO2
 
 from model.ctrv import predict_motion_from_ego_frame
 
+
 def create_ctrv_between_factor(key1, key2, vx, yaw_rate, delta_t, ctrv_config):
     """Create a BetweenFactor based on CTRV motion model.
+
+    Args:
+        key1:   Key to the first connected pose node.
+        key2 :  Key to the second connected pose node.
+            The keys should be generated using minisam.key() function.
+        vx:     Velocity in x. (m/s)
+        yaw_rate: Yaw rate. (rad/s)
+        delta_t:  Time difference. (sec)
+        ctrv_config (dict): Configurations for ctrv factor.
     """
     # Initizlize noise covariance matrix of vx and yaw_rate
     Q = np.zeros((2, 2))
