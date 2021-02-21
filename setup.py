badd1823371f57485e5944a8ac254763c5fb104c
Larry Liang <lian999111@gmail.com>
2021-02-21 17:13:22 +0100

    Add docstring
    Former-commit-id: 7a34307c31ead814fa6a62988aab9af7996b2487

--- setup.py
+++ setup.py
@@ -1,3 +1,10 @@
+"""Setup for installing the project locally.
+
+Once the project is install, the library can be impored by any scripts within this project.
+This makes testing more convenients.
+
+Ref: https://stackoverflow.com/questions/6323860/sibling-package-imports/50193944#50193944"""
+
 from setuptools import setup, find_packages
 
 setup(name='smmpda', version='1.0', packages=find_packages())
